792c4f6ce0a68028fd80647799ec187dc99253e1
Andrey Kyrdyumov <kant2002@gmail.com>
2017-06-26 00:07:22 +0600

    Add script for building NuGet packages

--- Build.bat
+++ Build.bat
@@ -1,1 +1,17 @@
-"%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" "%~dp0\NCrawler.proj" /t:Clean;Build;CopyToBinFolder;Merge;MergeNCrawlerConsole
+SET VersionSuffix=%1
+SET Configuration=Release
+mkdir out
+del /Q out\*.*
+dotnet msbuild "/t:Restore" /p:Version=4.0.0-%VersionSuffix% /p:VersionSuffix=%VersionSuffix% /p:Configuration=%Configuration%
+dotnet msbuild "/t:Pack" /p:Version=4.0.0-%VersionSuffix% /p:VersionSuffix=%VersionSuffix% /p:Configuration=%Configuration%
+
+copy /Y src\NCrawler\bin\%Configuration%\NCrawler.4.0.0-%VersionSuffix%.nupkg out
+copy /Y src\NCrawler.HtmlProcessor\bin\%Configuration%\NCrawler.HtmlProcessor.4.0.0-%VersionSuffix%.nupkg out
+copy /Y src\NCrawler.EsentServices\bin\%Configuration%\NCrawler.EsentServices.4.0.0-%VersionSuffix%.nupkg out
+copy /Y src\NCrawler.EntityFramework\bin\%Configuration%\NCrawler.EntityFramework.4.0.0-%VersionSuffix%.nupkg out
+copy /Y src\NCrawler.iTextSharpPdfProcessor\bin\%Configuration%\NCrawler.iTextSharpPdfProcessor.4.0.0-%VersionSuffix%.nupkg out
+copy /Y src\NCrawler.MP3Processor\bin\%Configuration%\NCrawler.MP3Processor.4.0.0-%VersionSuffix%.nupkg out
+copy /Y src\NCrawler.SitemapProcessor\bin\%Configuration%\NCrawler.SitemapProcessor.4.0.0-%VersionSuffix%.nupkg out
+copy /Y src\NCrawler.RedisServices\bin\%Configuration%\NCrawler.RedisServices.4.0.0-%VersionSuffix%.nupkg out
+copy /Y src\NCrawler.FileStorageServices\bin\%Configuration%\NCrawler.FileStorageServices.4.0.0-%VersionSuffix%.nupkg out
+copy /Y src\NCrawler.IFilterProcessor\bin\%Configuration%\NCrawler.IFilterProcessor.4.0.0-%VersionSuffix%.nupkg out
--- README.md
+++ README.md
@@ -6,3 +6,9 @@ Copy of NCrawler from http://ncrawler.codeplex.com/
 Simple and very efficient multithreaded web crawler with pipeline based processing written in C#. 
 Contains HTML, Text, PDF, and IFilter document processors and language detection(Google). 
 Easy to add pipeline steps to extract, use and alter information.
+
+## Build Nuget packages
+
+Create debug packages
+
+    Build.bat build002
